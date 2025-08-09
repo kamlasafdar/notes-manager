41e29c1f7c2abb811d56accd54877105f3f3312c
kamla safdar <kamlasafdar23@gmail.com>
2025-08-09 22:53:19 +0500

    basic setup of project

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold text-blue-400 mb-4">Tailwind is working 🎉</h1>
+      <button className="px-4 py-2 bg-blue-500 rounded hover:bg-blue-700">
+        Test Button
+      </button>
+    </div>
+  );
+}
+
+export default App;
